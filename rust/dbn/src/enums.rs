5c25a92e41d5338d50f967a288956964b5191698
Carter Green <carter@databento.com>
2023-06-02 13:26:54 -0500

    ADD: Add deserialize for enums

--- rust/dbn/src/enums.rs
+++ rust/dbn/src/enums.rs
@@ -590,3 +590,40 @@ pub enum StatUpdateAction {
     /// A removal of a statistic.
     Delete = 2,
 }
+
+#[cfg(feature = "serde")]
+mod deserialize {
+    use std::str::FromStr;
+
+    use serde::{de, Deserialize, Deserializer};
+
+    use super::*;
+
+    impl<'de> Deserialize<'de> for Compression {
+        fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+            let str = <&str>::deserialize(deserializer)?;
+            FromStr::from_str(str).map_err(de::Error::custom)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for SType {
+        fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+            let str = <&str>::deserialize(deserializer)?;
+            FromStr::from_str(str).map_err(de::Error::custom)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Schema {
+        fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+            let str = <&str>::deserialize(deserializer)?;
+            FromStr::from_str(str).map_err(de::Error::custom)
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Encoding {
+        fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+            let str = <&str>::deserialize(deserializer)?;
+            FromStr::from_str(str).map_err(de::Error::custom)
+        }
+    }
+}
